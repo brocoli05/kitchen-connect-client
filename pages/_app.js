01015dfaa7c2da4ed9ad77fefa1cbaf2e51b93cc
evelyn <etersh810@gmail.com>
2025-09-20 06:00:49 +0000

    change order of import css to apply css on mainpage

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '@/styles/globals.css';
 import '@/styles/main.css';
-import '@/styles/mainpage.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import '@/styles/mainpage.css';  
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
