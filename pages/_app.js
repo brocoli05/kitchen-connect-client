70a497c32f1c419be5f0a1856b95462aca911ca3
evelyn <etersh810@gmail.com>
2025-09-20 05:44:15 +0000

    import global css in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
-import "@/styles/globals.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import '@/styles/globals.css';
+import '@/styles/main.css';
+import '@/styles/mainpage.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
