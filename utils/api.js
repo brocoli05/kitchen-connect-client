5e2adeeff9af7484bb17d310812d4b805f9888c6
Minji Kim <mjkimthebest@gmail.com>
2025-09-23 17:05:28 -0400

    modify env for react

--- utils/api.js
+++ utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL
-  ? `${process.env.NEXT_PUBLIC_API_URL}/api`
+const API_BASE_URL = process.env.REACT_APP_API_URL
+  ? `${process.env.REACT_APP_API_URL}/api`
   : "http://localhost:5050/api";
 
 console.log("This is test-API_BASE_URL:", API_BASE_URL);
