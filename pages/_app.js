bc250d5f335115de297ad33cdaa82967260ca2e9
Minji Kim <mjkimthebest@gmail.com>
2025-11-08 13:05:05 -0500

    Add google account login feature

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,16 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '@/styles/globals.css';
-import '@/styles/main.css';
-import '@/styles/mainpage.css';  
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@/styles/globals.css";
+import "@/styles/main.css";
+import "@/styles/mainpage.css";
+import { SessionProvider } from "next-auth/react";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
